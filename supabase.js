f5fed25db6e2bbf0f9f32e3cba693287d5c9e68b
melodymimmim <mimjustbored@gmail.com>
2026-07-11 23:34:19 +0900

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-const supabaseUrl = "https://xtttlmnmyyjfwnnixdjg.supabase.co/rest/v1/";
-const supabaseKey = "sb_publishable_AP4bb00cmdJ8tntNsIT7sw_zbgCLemp";
-const supabaseClient = supabase.createClient(
-    supabaseUrl,
-    supabaseKey
-);
